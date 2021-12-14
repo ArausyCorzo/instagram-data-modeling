cf2351fb479c78952a92966a166cc349926777c3
ArausyCorzo <arausyncc@gmail.com>
2021-12-14 21:50:02 +0000

    yo no se si funciona esto pero bueh

--- src/models.py
+++ src/models.py
@@ -15,10 +15,61 @@ class Users(Base):
     name = Column(String(40), nullable = False)
     lastname= Column(String(40))
     email = Column(String(12), unique = True)
+    profile = relationship("Profile", back_populates = "parent")
    
-# class Profile(Base):
-#     __tablename__ = 'profile'
-#     biografy 
+class Profile(Base):
+    __tablename__ = 'profile'
+    id = Column(Integer, primary_key = True)
+    biografy = Column(String(300))
+    image = Column(String)
+    post = Column(String)
+    reel = Column(String)
+    story = Column(String)
+    followers = Column(Integer)
+    following = Column(Integer)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship("Users", back_populates = "children")
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key = True)
+    fav = Column(String)
+    
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key = True)
+    image = Column(String)
+    comment = Column(String)
+    city_adress = Column(String)
+    likes = Column(Integer)
+    profile_id = Column(Integer, ForeignKey("profile.id"))
+    save_id = Column(Integer, ForeignKey("save.id"))
+    save = relationship("child")
+
+class Reel(Base):
+    __tablename__ = 'reel'
+    id = Column(Integer, primary_key = True)
+    image = Column(String)
+    comment = Column(String)
+    likes = Column(Integer)
+    profile_id = Column(Integer, ForeignKey("profile.id"))
+    save_id = Column(Integer, ForeignKey("save.id"))
+    save = relationship("child")
+
+class Story(Base):
+    __tablename__ = 'story'
+    id = Column(Integer, primary_key = True)
+    views = Column(Integer)
+    profile_id = Column(Integer, ForeignKey("profile.id"))
+
+class Save(Base):
+    __tablename__ = 'save'  
+    id = Column(Integer, primary_key = True)
+    post = Column(String)
+    reel = Column(String)
+    favorites_id = Column(Integer, ForeignKey("favorites.id"))
+
 
     def to_dict(self):
         return {}
